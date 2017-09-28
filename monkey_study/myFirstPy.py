1265871c3a6dc796701acb60c348ea978de6c07b
monkey <itmonkey007@163.com>
2017-09-28 19:38:34 +0800

    day02

--- monkey_study/myFirstPy.py
+++ monkey_study/myFirstPy.py
@@ -1,1 +1,24 @@
-print("hello world,this is my first python program")
+#数据类型
+print('''i 
+... am 
+... Monkey''');
+#布尔类型
+age = 20;
+if age > 18:
+    print("adult");
+else:
+    print("teenager");
+#变量 动态变量
+monkeyA = 100;
+print("动态变量-->", monkeyA);
+monkeyA = "i like JWJ";
+print("动态变量-->", monkeyA);
+#两种除法
+#一种除法 /
+a = 10/3;
+print("一种除法 / --> 10 / 3 =", a);
+a = 9 / 3;
+print("一种除法 / --> 10 / 3 =", a);
+#另一种除法  整数除整数得整数   如java中的/  python中是//
+a = 10//3;
+print("另一种除法  整数除整数得整数  / --> 10 / 3 =",a);
