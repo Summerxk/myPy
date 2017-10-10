23b84cca64e7f5e5873c4059bb7a8dae59af6de2
18731788052@163.com <18731788052@163.com>
2017-10-10 23:15:30 +0800

    day06 end

--- monkey_study/day06myPy.py
+++ monkey_study/day06myPy.py
@@ -124,4 +124,62 @@ person('Monkey',25,**extra)
 #注意kw获得的dict是extra的一份拷贝，对kw的改动不会影响到函数外的extra
 print('================================')
 print('命名关键字参数')
+#对于关键字参数，函数的调用者可以传入任意不受限制的关键字参数。至于到底传入了哪些，就需要在函数内部通过kw检查
+#如
+def person(name,age,**kw):
+    if 'city' in kw:
+        print('city = ',kw)
+    if 'job' in kw:
+        print('job = ',kw)
+    print('name = ',name,'age = ',age,'other = ',kw)
+person('Monkey',25,city = 'GZ',job = 'engineer')
+person('Jack', 24, city='Beijing', addr='Chaoyang', zipcode=123456)
+#如果要限制关键字参数的名字，就可以用命名关键字参数，例如，只接收city和job作为关键字参数。这种方式定义的函数如下
+def person(name,age,*,city,job = 'Engineer'):
+    print(name,age,city,job)
+#person('Jack', 24, 'Beijing', 'Engineer')
+person('Jack', 24, city='Beijing', job='Engineer')
+person('Jack', 24, city='Beijing')
+#如果函数定义中已经有了一个可变参数，后面跟着的命名关键字参数就不再需要一个特殊分隔符*了
+#def person(name, age, *args, city, job):
+ #   print(name, age, args, city, job)
+ #命名关键字参数必须传入参数名，这和位置参数不同。如果没有传入参数名，调用将报错
 
+ #使用命名关键字参数时，要特别注意，如果没有可变参数，就必须加一个*作为特殊分隔符。如果缺少*，Python解释器将无法识别位置参数和命名关键字参数
+ 
+print('================================')
+print('参数组合')
+#在Python中定义函数，可以用必选参数、默认参数、可变参数、关键字参数和命名关键字参数，这5种参数都可以组合使用。但是请注意，
+#参数定义的顺序必须是：必选参数、默认参数、可变参数、命名关键字参数和关键字参数。
+def f1(a,b,c=0,*args,**kw):
+    print('a = ',a,'b = ',b,'c = ',c,'args = ',args,'kw = ',kw)
+
+def f2(a,b,c = 0,*,d,**kw):
+    print('a = ',a,'b = ',b,'c = ',c,'d = ',d,'kw = ',kw)
+
+args = (1, 2, 3, 4)
+kw = {'d': 99, 'x': '#'}
+f1(*args, **kw)
+args = (1, 2, 3)
+kw = {'d': 88, 'x': '#'}
+f2(*args, **kw)
+#对于任意函数，都可以通过类似func(*args, **kw)的形式调用它，无论它的参数是如何定义的
+f1(1, 2)
+f1(1, 2, c=3)
+f1(1, 2, 3, 'a', 'b')
+f1(1, 2, 3, 'a', 'b', x=99)
+f2(1, 2, d=99, ext=None)
+
+#小结
+#Python的函数具有非常灵活的参数形态，既可以实现简单的调用，又可以传入非常复杂的参数。
+#默认参数一定要用不可变对象，如果是可变对象，程序运行时会有逻辑错误！
+#要注意定义可变参数和关键字参数的语法：
+#*args是可变参数，args接收的是一个tuple；
+#**kw是关键字参数，kw接收的是一个dict。
+#以及调用函数时如何传入可变参数和关键字参数的语法：
+#可变参数既可以直接传入：func(1, 2, 3)，又可以先组装list或tuple，再通过*args传入：func(*(1, 2, 3))；
+#关键字参数既可以直接传入：func(a=1, b=2)，又可以先组装dict，再通过**kw传入：func(**{'a': 1, 'b': 2})。
+#使用*args和**kw是Python的习惯写法，当然也可以用其他参数名，但最好使用习惯用法。
+#命名的关键字参数是为了限制调用者可以传入的参数名，同时可以提供默认值。
+#定义命名的关键字参数在没有可变参数的情况下不要忘了写分隔符*，否则定义的将是位置参数。
+ 
