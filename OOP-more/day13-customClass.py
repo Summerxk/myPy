ec94b38b895b66a166a55e12c187b65594dcd542
xiakai <xiakai@microgravity.cn>
2017-10-25 21:38:27 +0800

    customClass end

--- OOP-more/day13-customClass.py
+++ OOP-more/day13-customClass.py
@@ -97,7 +97,7 @@ print('f[:10:2] ---->',f[:10:2])
 #总之，通过上面的方法，我们自己定一个的类的表现形式，和python 自带的list tuple,dict没有什么区别，这完全是
 #归功于动态语言的‘鸭子类型’，不需要强制继承某一个接口。
 
-#__getattr__
+#4.__getattr__
 print('-----------__getattr__()-------------')
  #正常情况下，如果我们调用类的某个属性和方法，如果没有的话，就会报错，比如定义 Student类：
 class Student(object):
@@ -124,3 +124,58 @@ class Student(object):
     def __getattr__(self, attr):
         if attr=='age':
             return lambda: 25
+
+#只要调用的方式变为
+s = Student()
+s.age()
+
+#注意，只要在没有找到属性的情况下，才调用__getattr__，已有的属性，比如name,不会在__getarrt__中查找。
+#此外，注意到任意调用如，s.abc 都会返回None,这是因为我们定义的__getattr__默认返回的就是None.要让class只响应特定的几个属性，我们就按照
+#约定，抛出 AttributeError的错误
+class Student(object):
+    
+    def __getattr__(self, attr):
+        if attr=='age':
+            return lambda: 25
+        raise AttributeError('\'Student\' object has no attribute \'%s\'' % attr)
+
+#5.__call__ 
+print('----------__call__---------------')
+#一个对象实例可以有自己的属性和方法，当我们调用实例方法的时候，我们用 instance.merthod()来调用。也可以用实例本身上进行调用
+class Student(object):
+    def __init__(self,name):
+        self.name = name
+    def __call__(self):
+        print('my name is %s' % self.name)
+
+s = Student('summer')
+s()
+#__call__() 还可以定义参数，对实例进行直接调用就好比对一个函数进行调用一样，所以你完全可以把对象看成函数，把对象堪称函数。两者直接就没有根本的区别
+
+#but 我们把对象看成函数，那么函数本身其实也可以在运行期间动态的创建出来，因为类的实例都是运行期创建出来的，这样，我们就模糊了对象和函数的界限。
+
+#so 我们需要判断一个对象是否能被调用，能被调用的对象就是一个 Callable对象，比如函数和我们上面定义的带有 __call__() 的类的实例
+
+print('callable:student--->',callable(Student('summer')))#True
+print('callable:max--->',callable(max))#True
+print('callable:[1,2,3]--->',callable([1,2,3]))#False
+print('callable:None--->',callable(None))#False
+print('callable:str--->',callable('str'))#False
+#通过callable()函数，可以判断一个对象是否是‘可调用’对象
+
+print('更多可被调用对象 doc 文档')
+#https://docs.python.org/3/reference/datamodel.html#special-method-names
+
+#总结：
+#1，__str__(): 相当于 toString吧
+#2，__iter__ 要于 __next__() 结合可进行 for... in循环
+#3，__getitem__() 可以按照下标进行取出元素，还有切片的处理，但是不能完全 相当于list 比如说下标为负数，或者 没有对 step参数做处理
+#4，__getattr__() 对未定义的属性 进行默认赋值
+#5，__call__() 实例本身当作函数进行调用
+#6，还有很多 https://docs.python.org/3/reference/datamodel.html#special-method-names 回头细看
+
+print('-------------------分割线-------------------------------')
+
+print('------------next--------------')
+
+
